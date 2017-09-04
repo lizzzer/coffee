9c7b91aa0d8a671dad855310c8740c12f8bab82a
lizzzer <janne.kalliokorpi@gmail.com>
2017-09-04 15:33:07 +0300

    new api

---
+++ api/controllers/StatsController.js
@@ -0,0 +1,38 @@
+var moment = require('moment');
+
+/**
+ * StatsController
+ *
+ * @description :: Server-side logic for managing Stats
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+	doShowMachineMaintenance: function(req, res) {
+
+		var params = req.params.all();
+
+		MachineService.doGetMachineHistory(function(err, hist) {
+
+			json = hist;
+			json.history = hist;
+			json.moment = moment;
+
+			return res.view('stat_history', json);
+
+		});
+
+	},
+
+	doShowPoints: function(req, res) {
+
+		var params = req.params.all();
+
+		UserService.doGetAllUsers(function(err, resp) {
+			json = resp;
+			return res.view('stat_points', json);
+		});
+	}
+
+};
