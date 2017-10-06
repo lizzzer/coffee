985478b76d20ce6210cb0be7cc398465a2146c4a
zeikei <janne.kalliokorpi@gofore.com>
2017-10-06 10:37:28 +0300

    new tasks

--- config/custom.js
+++ config/custom.js
@@ -15,6 +15,9 @@ module.exports.custom = {
     maintenance_9: {section:"1", action_id:1, machine_id:'4', points:5, pageTitle:'Keitin kahvia', messageToBroadcast:' keitti juuri kahvia', messageToEndPoint:'Kahvia tulossa :)', maintenance_id:9, history_message:'Kahvia keitetty', message: 'Kerroin kaikille!', timeout:1},
     maintenance_10: {section:"1", action_id:2, machine_id:'4', points:5, pageTitle:'Teetä hautumassa', messageToBroadcast:' hauduttelee teetä', messageToEndPoint:'Teetä hautumassa :)', maintenance_id:10, history_message:'Teetä pistetty', message: 'Kerroin kaikille!', timeout:1},
     maintenance_11: {section:"1", action_id:3, machine_id:'4', points:5, pageTitle:'Hain paketin', messageToBroadcast:' kävi hakemassa paketin aulasta', messageToEndPoint:'Kävin hakemassa paketin aulasta', maintenance_id:11, history_message:'Paketti haettu', message: 'Kerroin kaikille!', timeout:1},
+    maintenance_12: {section:"1", action_id:4, machine_id:'4', points:10, pageTitle:'Tein smoothieta', messageToBroadcast:' tekee smoothieta', messageToEndPoint:'Pyöräytin smoothien! Tulukee maistamaan', maintenance_id:12, history_message:'Smoothieta tehty', message: 'Kutsuin ystäviä!', timeout:1},
+    maintenance_13: {section:"2", action_id:5, machine_id:'3', points:10, pageTitle:'Siivosin neukkarin', messageToBroadcast:' siivosi neukkarin', maintenance_id:13, history_message:'Neukkari siivottu', message: 'Nyt on taas kiva palaveerata', timeout:1},
+
 
     notify_2: {section:"2", points: 0, pageTitle:'Juon kaljaa', messageToBroadcast:' juo olutta prkl!', messageToEndPoint:' juo olutta, and damn it good! _Ede, bibe, lude, post mortem nulla voluptas._', notify_id:2, message: 'Kutsuin kavereita! Ede, bibe, lude, post mortem nulla voluptas.'}
 
